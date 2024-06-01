700d4897f0ef56284ba13a0da3f78f851e5f7847
EberG25 <168913317+EberG25@users.noreply.github.com>
2024-06-01 00:10:49 +0000

    projec_finish

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function randomItem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+console.log("Hello Rigo from the console!");
+const whos = ["my boss", "brother", "my dog"];
+const whats = ["lost", "eat", "block"];
+const avoids = ["my check", "my shoe", "computer"];
+const whens = ["yesterday", "today", "last week"];
+
+const who = randomItem(whos);
+const what = randomItem(whats);
+const avoid = randomItem(avoids);
+const when = randomItem(whens);
+
+const randomExcuse = `${who} ${what} ${avoid} ${when}`;
+document.querySelector("div").innerText = randomExcuse;
